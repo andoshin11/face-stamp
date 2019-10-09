8083178d77d72a9920f498eb322bb27487773a03
andoshin11 <shinglish11@gmail.com>
2019-10-09 16:10:22 +0900

    Add limit and improved success feedback

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import { GetEmojiList, GetUserList } from './api/requests/Slack'
 
 dotenv.config()
 
+const LIMIT = 30
+
 const main = async () => {
   const client = new APIClient()
 
@@ -27,20 +29,25 @@ const main = async () => {
   )
 
   try {
-    console.log(`Registering ${unregistered.length} users...`)
+    let compact = unregistered
+    if (unregistered.length >= LIMIT) {
+      console.log(`Only ${LIMIT} users can be added at one time`)
+      compact = compact.slice(0, LIMIT)
+    }
+    console.log(`Registering ${compact.length} users...`)
     const response = await emojme.add(
       [process.env.SLACK_DOMAIN!],
       [process.env.SLACK_USER_TOKEN!],
       {
-        src: unregistered.map(user => user.profile.image_72),
-        name: unregistered.map(user => user.name.replace(/\./, '_')),
+        src: compact.map(user => user.profile.image_72),
+        name: compact.map(user => user.name.replace(/\./, '_')),
         bustCache: false,
         avoidCollisions: true,
         output: true
       }
     )
     console.log('Done')
-    console.log(JSON.stringify(response))
+    console.log(response[process.env.SLACK_DOMAIN!].emojiList.map(i => i.name).join('\n'))
   } catch (e) {
     console.log('error')
     console.log(e)
